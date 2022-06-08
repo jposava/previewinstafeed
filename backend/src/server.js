e2a6e40340b6b7e6167bdd4e232bcc14591f88e5
João Pedro Osava <jposava@gmail.com>
2022-06-08 12:04:24 -0300

    primeiro commit para o backend

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+const port = 8080;
+const token = "IGQVJYTEx4cVdNNVJ5SnNNQ2RjdTB5TzRzQWI4OWxOd3NxeWxDVVo0S3AwcW1yaTctSGYzZAk5zSEl1MkRIS1pIelZA1SzlZAVlNzWVd6Y1dJa3c0S2ZACZAGZAOTktMNTZAUeUxGNkhmakUwVmNyaERfTEJjNQZDZD";
+const part2url = "&fields=media_url,media_type,caption,permalink";
+const url = `https://graph.instagram.com/me/media?access_token=${token + part2url}`;
+
+//SERVER ON
+server.listen(port, () => {
+    console.log(`API ONLINE NA PORTA ${port}`);
+});
+
+//GET FEED
+server.get('/', async (req, res) => {
+    return (
+        res.status(200)
+            .send(await axios.get(url)
+                .then((res) => res.data))
+    );
+});
