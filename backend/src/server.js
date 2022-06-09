bb8ad665282cba7a31ce7da47f9e510657cb9527
João Pedro Osava <jposava@gmail.com>
2022-06-09 12:50:35 -0300

    adicionando .env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
 const axios = require('axios');
@@ -8,9 +10,8 @@ server.use(express.json());
 server.use(cors());
 
 const port = 8080;
-const token = "IGQVJYTEx4cVdNNVJ5SnNNQ2RjdTB5TzRzQWI4OWxOd3NxeWxDVVo0S3AwcW1yaTctSGYzZAk5zSEl1MkRIS1pIelZA1SzlZAVlNzWVd6Y1dJa3c0S2ZACZAGZAOTktMNTZAUeUxGNkhmakUwVmNyaERfTEJjNQZDZD";
 const part2url = "&fields=media_url,media_type,caption,permalink";
-const url = `https://graph.instagram.com/me/media?access_token=${token + part2url}`;
+const url = `https://graph.instagram.com/me/media?access_token=${process.env.TOKEN + part2url}`;
 
 //SERVER ON
 server.listen(port, () => {
@@ -22,3 +23,6 @@ server.get('/', async (req, res) => {
     const data = await axios.get(url).then((res) => res.data)
     return  res.status(200).send(data)
 });
+
+
+//mongodb+srv://jposava:<password>@cluster0.fbkeb.mongodb.net/?retryWrites=true&w=majority
